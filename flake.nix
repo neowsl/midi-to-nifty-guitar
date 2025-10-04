320628e93a59d2557ab5d219319f1dad5a932167
Neal Wang <nealwang.sh@protonmail.com>
2025-10-03 18:06:14 -0700

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python3.withPackages (
+          ps: with ps; [
+            mido
+          ]
+        );
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            pythonEnv
+          ];
+        };
+      }
+    );
+}
