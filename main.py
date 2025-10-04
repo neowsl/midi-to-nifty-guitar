5af64468a369cb8771e3cc1f1bc28ba787bd6447
Neal Wang <nealwang.sh@protonmail.com>
2025-10-03 22:03:46 -0700

    chore: move files to root

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
 
     args = parser.parse_args()
 
+    # load MIDI file
     mid = mido.MidiFile(args.input_file)
 
     wait = 0
@@ -21,9 +22,12 @@ def main():
     for msg in mid:
         wait += msg.time
 
-        if msg.type != "note_on" or msg.velocity <= 10:
+        # don't include events other than starts of notes
+        # or where note is too soft
+        if msg.type != "note_on" or msg.velocity <= 0:
             continue
 
+        # maps concert A to 0
         events.append((msg.note - 69, wait))
         wait = 0
 
